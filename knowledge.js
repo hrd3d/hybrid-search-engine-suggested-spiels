2942f7cde6f933cd4bc3f9972bfedc2258b5ad36
Hon III Dal <hdalaguitiii@gmail.com>
2026-05-09 18:38:40 +0800

    Update knowledge.js

--- knowledge.js
+++ knowledge.js
@@ -1,79 +1,104 @@
 const knowledgeBase = `
 
 EKB ACKNOWLEDGEMENT
+EKB acknowledgement link:
 https://lookerstudio.google.com/u/0/reporting/1045e3d1-d495-4f86-a943-f32efaed2797/page/p_61jhz927zd?pli=1 
 
 END STATUS TAGGING 
+End status tagging link:
 https://docs.google.com/forms/d/e/1FAIpQLSeCAeJ2QCNRM11mPO3yTkn7EsulUEKyT871NVo1WQvp4tiC0w/viewform 
 
 EXPEDITE RETURN & REFUND FORM
+Expedite return & refund form link:
 https://docs.google.com/forms/d/e/1FAIpQLSeeZf-ROk1yTKUJmAFyov59qvWSZEy0ysBNns1XYySS736YHw/viewform?pli=1&pli=1 
 
 ERR CHECK FORM
+ERR check form link:
 https://docs.google.com/spreadsheets/d/15zzjGc731LdpYVZgEicrT2KrppvrBa9QQs4yfQgvHj4/edit?gid=764061354#gid=764061354 
 
 SPX BACKLOG TRACKER
+SPX backlogs tracker link:
 https://lookerstudio.google.com/reporting/525613f8-d08b-408d-8752-7d7c35e470a5/page/1AV9D  
 
 ARN LOCATOR v2 (credit card/debit card)
+ARN locator v2 (credit card/debit card) link:
 https://lookerstudio.google.com/u/0/reporting/9de0e4cd-c4f1-464b-99fc-a31c1acfe0e0/page/p_xzmh29cwud 
 
 3PL MATRIX
+3PL matrix link:
 https://docs.google.com/spreadsheets/d/1FW-T4Q-fD6ShQ33x5B9btBsa7sOZGaqHS8xi0TeZmzw/edit?gid=0#gid=0 
 
 SHOPEE GPT
+Shopee GPT link:
 https://chatgpt.com/g/g-Xe8waS9NP-shopee-gpt-demo 
 
 OTHER SPIEL
+Other spiel link:
 https://docs.google.com/spreadsheets/d/1aHo3PtTDa12JxFMxJakweKydMFC9JyaXQ3W3msB-p0g/edit?gid=0#gid=0 
 
 IDLE SPIEL
+Suggested spiel:
 If this chat gets disconnected, we can continue our conversation through the Shopee In-app case tracker. To view your recent interactions, please go to the "Me" tab, then click on "Help Center," "Top Ranked Enquires," and "My Inquiries."
 
 SURVEY SPIEL 
+Suggested spiel:
 I’m glad I could help you today. After po ng chat na ito, may lalabas pong short survey tungkol sa support na na-provide. If you have time po, we’d appreciate your honest feedback.
 
 TRANSFER SPIEL SUPERVISOR
+Suggested spiel:
 I completely understand how important this matter is for you. To make sure we provide you with the most accurate resolution, I will now connect you with my supervisor who can further assist you. Kindly stay on the line while I transfer the chat. Thank you for your understanding.
 
 FOR SOFT ESCALATION
+Suggested spiel:
 I understand that the provided solution today does not meet your expectations po, We apologize po sa abala. To ensure we provide the most accurate support po. we’ve asked a supervisor to review your concern for more options. We’ll get back to you within 24 hours with an update.
 
 SOFT ESCALATION
+Guide:
 Internal Note: Soft Escalation raised to Michelle Taparan 
 Place the case on Hold Case Status
 
 TRANSFER SPIEL TO THE RIGHT DEPARTMENT
+Suggested spiel:
 I understand that your concern is about —. For further assistance po, I will transfer you to our assigned team. 
 
 DISCONNECT
+Suggested spiel:
 I was momentarily disconnected from the chat, but I’m here now and ready to assist you. Let me go ahead and continue checking the details for you.
 
 DE-ESCALATION SPIEL
+Suggested spiel:
 I acknowledge your request to speak with a supervisor regarding your concern, po. However, I respectfully ask for the opportunity to address the matter myself po. Please be assured that I will make every effort to resolve the issue to your satisfaction, po.
 
 ESCALATED
+Suggested spiel:
 We already escalated this concern po to the assigned team. Please anticipate the response po that we will be able to get back to you within the next 24 to 48 hours. Kindly keep this reference number #{Case ID} if you want to follow up.
 
 MULTIPLE XFER
+Suggested spiel:
 Pasensya na po if you have been transferred multiple times already. Para matulungan namin kayo, let me connect you to my supervisor for additional support. Please stay on the line and thank you for your patience po. 
 
 CASE ID SUGGESTED SPIEL #1
+Suggested spiel:
 Ito po ang ating reference number #{Case ID} para po sa concern today. This will serve as your case ID if you would want to make a follow up, makikita po ng next agent lahat ng pinag-usapan natin maging sa mga previous agent na nakausap niyo sa case ID na ito.  
 
 CASE ID SUGGESTED SPIEL #2
+Suggested spiel:
 Naiintindihan ko po ang concern ninyo tungkol sa escalation reference. Please allow me to clarify po na sa Shopee system, wala po pong hiwalay na reference number para sa escalation. Ang case ID/reference number po natin #{Case ID}ang parehong ginagamit bilang reference para sa conversation at sa escalation.
 
 CASE ID SUGGESTED SPIEL #3
+Suggested spiel:
 Makikita po lahat ng pinag-usapan natin dito sa case thread, pati na rin ang mga follow-up at escalation na ginawa, kaya maaari niyo pong gamitin ang case ID na ito sa susunod na follow-up para makita ng next agent ang buong history ng concern niyo.
 
 FALSE INFO BY PREV AGENTS
+Suggested spiel:
 Rest assured po, all chat interactions are properly recorded. Any misinformation or incorrect information provided handled by the assigned team for proper investigation and corrective action.
 
 ESCALATED
+Suggested spiel:
 We already escalated this concern po to the assigned team. Please anticipate the response po that we will be able to get back to you within the next 24 to 48 hours. Kindly keep this reference number #{Case ID} if you want to follow up.
 
-FOR EXPEDITE
+EXPEDITE DELIVERY REQUEST
+For expedite
 Order SN:  
 Reason: 
 Detailed Concern: (Please prioritize team)
@@ -102,20 +127,23 @@ Related Party Owner: zbragt76@shopeemobile-external.com
 •Preferred voucher amount:
 •Detailed summary of the customer's concern:
 
-SPIEL FOR SR COMPENSATION 
-As a token of apology, we can process the 50 pesos compensation voucher for the inconvenience.
+SR COMPENSATION 
+Spiel for SR Compensation
+•As a token of apology, we can process the 50 pesos compensation voucher for the inconvenience.
 I completely understand how this is, especially since it wasn’t your fault. I truly understand where you’re coming from. While I know the ₱50 voucher cannot fully make up for the inconvenience, it’s meant as a small token of apology.
-We've requested a voucher to be added to your Shopee account within 48-72 hours. Once approved, you can easily find the details in the 'Me' tab of your Shopee app under 'My Vouchers.'
+•We've requested a voucher to be added to your Shopee account within 48-72 hours. Once approved, you can easily find the details in the 'Me' tab of your Shopee app under 'My Vouchers.'
 
-RIDER REPORT SPX ONLY
-Run Macro: Wrong on hold/Fake delivery attempt
-Details of the complaint raised: The customer was not contacted by the rider yet the order was tagged as failed delivery due to —
+RIDER REPORT SPX
+Rider report SPX only
+•Run Macro: Wrong on hold/Fake delivery attempt
+•Details of the complaint raised: The customer was not contacted by the rider yet the order was tagged as failed •delivery due to —
 
 RIDER REPORT OTHER 3PL
-Run Macro: Logistics - Rider Behavior (Other 3PLs)
-Last Mile Tracking Number: #{Shipping ID}
-Additional info: The customer was not contacted by the rider yet the order was tagged as failed delivery due to —
-Deadline: +3days
+Rider report other 3PL
+•Run Macro: Logistics - Rider Behavior (Other 3PLs)
+•Last Mile Tracking Number: #{Shipping ID}
+•Additional info: The customer was not contacted by the rider yet the order was tagged as failed delivery due to —
+•Deadline: +3days
 
 RTS ORDER FOR REDELIVERY 3PL
 Run Macro: Logistics - Redelivery Request (RTS)
@@ -123,29 +151,33 @@ Run Macro: Logistics - Redelivery Request (RTS)
 CX WANTS TO CANCEL AT LM HUB WITH NO ATTEMPT AT ALL (SPX ONLY) 
 Request to Return
 
-CX ASK HOW TO FIND THE CASE TRACKER
+THE CASE TRACKER
+CX ask how to find the case tracker
 •Via help center:
 •In the Shopee app, go to the Me tab > Help Centre > Case Tracker (bottom section) to view the Case Tracker..
 •Via notification page:
 •Go to the Notification page > Tap Shopee Updates
 
 IN-APP CANCELLATION
+Shopee In-App cancellation
 •For in-app cancellation request by following the steps below:
 •On the "Me" tab, tap “To Ship”.
 •Under “To Ship”, tap the order that the buyer wants to cancel.
 •Tap 'Cancel Order' and select the reason for cancellation.
 •Tap the 'Confirm' button
 
-HERE'S HOW YOU CAN VIEW THE DISCUSS PAGE FOR YOUR RETURN/REFUND REQUEST IN THE SHOPEE APP
+VIEW DISCUSS PAGE FOR RETURN/REFUND REQUEST
+Here's how you can view the discuss page for your return/refund request in teh Shopee App:
 •Open the Shopee app.
 •Tap “Me” at the bottom right of the screen.
 •Tap “To Receive” or “Returns/Refunds” (depending on the order’s status).
 •Find and select the order you requested a return/refund for.
 •Tap “Return/Refund” — this will open the details of your request.
 •Scroll down and tap “Chat” or “View Discussion” (sometimes labeled as “Discuss” or “View Case”).
-•For more information you may visit this link: https://help.shopee.ph/portal/4/article/81432.
+•For more information you may visit this link: https://help.shopee.ph/portal/4/article/81432
 
-HOW TO ADD INFORMATION IN THE DISCUSS PAGE:
+INFORMATION IN THE DISCUSS PAGE
+How to add information in the discuss page:
 •Open the Shopee app
 •Log into your account.
 •Go to “Me” tab (bottom right corner)
@@ -160,12 +192,37 @@ HOW TO ADD INFORMATION IN THE DISCUSS PAGE:
 •Send your message by tapping the send icon (paper plane).
 
 INTERNET CALL FEATURE
-Internet Call (VOIP) is available to select users. This option can be seen via the Help Center in the Shopee app.  
+Internet Call (VOIP) is available to selected users. This option can be seen via the Help Center in the Shopee app.  
 •Go to Me tab.
 •Select Help Centre.
 •Tap Internet Call at the bottom of the Help Centre page.
 •Select if you are a Buyer or a Seller 
 •Select the concern and you’ll be redirected to Shopee’s Customer Service.
+
+FAQ LINKS 
+FAQ Links
+Cancel an order https://help.shopee.ph/portal/article/81180
+Cancel Request R/R https://help.shopee.ph/portal/article/81338
+Change delivery details https://help.shopee.ph/portal/article/81134
+Check RR status https://help.shopee.ph/portal/article/83687
+Delivered but not received https://help.shopee.ph/portal/article/106841
+Dispute status https://help.shopee.ph/portal/article/81286
+Failed/Incomplete pick up https://seller.shopee.ph/edu/article/3729
+How to check order status https://seller.shopee.ph/edu/article/3732
+Order was canceled https://help.shopee.ph/portal/article/104737
+Proof of delivery https://help.shopee.ph/portal/article/81287
+Refund for canceled order https://help.shopee.ph/portal/article/86983
+Return parcel Shipping https://help.shopee.ph/portal/article/126380
+Request R/R https://help.shopee.ph/portal/4/article/81231
+Refund Shipping Fee https://help.shopee.ph/portal/article/81321
+
+
+
+
+
+
+
+
 
 
 
